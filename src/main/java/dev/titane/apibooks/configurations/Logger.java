87fbe5f76c98114057efca1445c24d7836998416
Houssem SALEM <houssem.salem@wecraft.tn>
2023-03-08 20:36:39 +0100

    feat: add loggerÃ

---
+++ src/main/java/dev/titane/apibooks/configurations/Logger.java
@@ -0,0 +1,20 @@
+package dev.titane.apibooks.configurations;
+
+import org.iode.springbootsocle.filters.LoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Logger {
+
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> loggingFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
+    }
+}
